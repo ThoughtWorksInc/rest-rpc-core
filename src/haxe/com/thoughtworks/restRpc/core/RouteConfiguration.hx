c8308b2c7c73fdbd7ded0a56adf2e9205c9978ea
Suo Qin <kpsesuo@gmail.com>
2015-08-21 17:42:47 +0800

    change signature of routeConfiguration

--- src/haxe/com/thoughtworks/restRpc/core/RouteConfiguration.hx
+++ src/haxe/com/thoughtworks/restRpc/core/RouteConfiguration.hx
@@ -1,18 +1,13 @@
 package com.thoughtworks.restRpc.core;
 
-class RouteConfiguration {
-    public var nameToUriTemplate: Map<String, UriTemplate>;
+@:nativeGen
+interface RouteConfiguration {
+    public function nameToUriTemplate(name: String): UriTemplate;
+
 }
 
-class UriTemplate {
-    private var template: String;
-    private var verb: String;
-
-    public function new(template:String) {
-        this.template = template;
-    }
-
-    public function render(context: Dynamic):String{
-        return new haxe.Template(template).execute(context);
-    }
+@:nativeGen
+interface UriTemplate {
+    public function method(): String;
+    public function render(context: Dynamic):String;
 }
